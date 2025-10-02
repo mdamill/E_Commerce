62609c3574faa43ebd63412451b02bb950665825
mdamill <losthts8@gmail.com>
2025-10-02 21:40:34 +0530

    some util code

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,21 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import connectDB from './DB/index.js';
+import bodyParser from 'express'
+import cors from 'cors';
 
 dotenv.config(); // this allows access from the '.env'
 
 const app = express();
+
+// util code
+app.use(bodyParser.json())
+app.use(cors({
+  origin:true,
+  methods:[ "GET","POST","PUT","DELETE"],
+  credentials:true
+}))
+
 
 app.get('/ping', (req, res) => {
     res.send(`PONG`)
