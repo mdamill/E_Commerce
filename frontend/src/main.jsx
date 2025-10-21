b2399523dbc385939ed16195e6d081e03a9b5643
mdamill <losthts8@gmail.com>
2025-10-21 12:04:49 +0530

    waiting pushes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import AppState from './Context/AppState.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <AppState>
     <App />
-  </StrictMode>,
+  </AppState>
 )
